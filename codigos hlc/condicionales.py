c9703108ef925a815046b3db8821c012ac54730f
Jared20 <68714490+Jared20@users.noreply.github.com>
2020-09-26 09:14:31 -0600

    Add files via upload

---
+++ codigos hlc/condicionales.py
@@ -0,0 +1,147 @@
+#edad = int(input("Ingrese su edad"))
+
+#if edad >=18:
+ #   print("usted es mayor de edad")
+
+#else:
+#    print("usted no es mayor de edad")
+
+#a = int(input("Ingrese un número"))
+
+#if a > 0:
+    #print("El número es positivo")
+#else:
+    #if a < 0:
+      #  print("El número es negativo")
+    #else:
+     #   print("El número es 0")
+     
+#a = int (input ("Ingrese un número: "))
+
+#if a > 0:
+#	print ("El número es positivo")
+
+#elif a < 0:
+  #      print ("El número es negativo")
+
+#else:
+ #       print ("El número es cero")
+
+#import pyautogui
+
+#comand = input("Ingrese comando: ")
+
+#if comand == "screenshot":
+ #   pyautogui.screenshot("captura.png")
+  #  print("La captura se ha realizado con éxito")
+    
+#else:
+ #   print("comando ingresado incorrecto.")
+
+#print("El programa ha finalizado")
+
+#num1 = float(input("Ingrese un número: "))
+#num2 = float(input("Ingrese un número: "))
+
+#print("MENU OPERACIONES")
+#print("""1.Suma
+#2.Resta
+#3.Multiplicación""")
+
+#opc = input("Ingrese opción: ")
+
+#if opc == "1":
+    #suma = num1+num2
+   # print(suma)
+#elif opc == "2":
+  #  resta = num1-num2
+ #   print(resta)
+#elif opc == "3":
+ #   producto = num1*num2
+  #  print(producto)
+#else:00
+ #   print("Opción ingresada incorrecta")
+#print("El programa ha finalizado")
+
+#for i in range(1,5,2):
+   # print(i)
+    
+#for i in range(1,21):
+ #   if i % 2 == 0:
+  #      print(i)
+
+#li =  int(input("Ingrese limite inferior:"))
+
+#for i in range(li,21):
+ #   if i % 2 == 0:
+  #      print(i)
+
+#i = 0
+#while i < 10:
+ #   print(i)
+  #  i = i + 1
+
+#nuevalista = [1,2,True,False,"cadena",[6,7]]
+
+#print(nuevalista)
+
+#nuevalista.append(4)
+#nuevalista.append(True)
+#nuevalista.append('asdsada')
+#nuevalista.append([5,True])
+
+#print(nuevalista)
+
+#print("Los métodos que puedes con una lista es el siguiente")
+#print(dir(nuevalista))
+
+#print("Eliminando un elemento")
+#nuevalista.pop()
+#print(nuevalista)
+
+#nuevalista = [1,2,True,False,"cadena",[1,2]]
+#numero = 0
+
+#for i in nuevalista:
+ #   numero +=1
+
+#print("El tamaño de la lista es:",len(nuevalista))
+
+nuevalista = []
+
+for i in range(1,4):
+    a = input("Ingresa un valor")
+    nuevalista.append(a)
+
+print(nuevalista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+	
+
+
